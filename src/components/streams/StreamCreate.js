bb6f8839804dff9e79d47bcbcdb50d5d09f6c82d
Himani <himani9.engg@gmail.com>
2022-02-01 14:51:36 +0530

    validation of form inputs

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -33,6 +33,18 @@ class StreamCreate extends React.Component {
     );
   }
 }
+
+const validate = (formValues) => {
+  const errors = {};
+  if (!formValues.title) {
+    errors.title = "you must enter a title";
+  }
+  if (!formValues.description) {
+    errors.description = "you must enter a description";
+  }
+  return errors;
+};
+
 export default reduxForm({
   form: "streamCreate",
 })(StreamCreate);
