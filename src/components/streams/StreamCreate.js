988080b31281f9725989c13b3fea86eb89517c42
Himani <himani9.engg@gmail.com>
2022-02-01 13:33:28 +0530

    creating forms

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,6 +1,25 @@
 import React from "react";
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
-};
+import { Field, reduxForm } from "redux-form";
 
-export default StreamCreate;
+class StreamCreate extends React.Component {
+  renderInput(formProps) {
+    console.log(formProps);
+    return (
+      <input
+        onChange={formProps.input.onChange}
+        value={formProps.input.value}
+      />
+    );
+  }
+  render() {
+    return (
+      <form>
+        <Field name="title" component={this.renderInput} />
+        <Field name="description" component={this.renderInput} />
+      </form>
+    );
+  }
+}
+export default reduxForm({
+  form: "streamCreate",
+})(StreamCreate);
